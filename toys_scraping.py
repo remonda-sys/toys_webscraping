b8fa7de3cccaf3b7ee407ee8050961d4bfe7b16a
remonda-sys <93402526+remonda-sys@users.noreply.github.com>
2022-12-19 11:26:44 +0200

    Update toys_scraping.py

--- toys_scraping.py
+++ toys_scraping.py
@@ -8,9 +8,7 @@ data_string = ""
 
 while page_num != 29:
     result=requests.get(f"https://www.amazon.eg/s?k=toys&i=toys&rh=n%3A21860119031&page=3&language=en&qid=1671358382&ref=sr_pg_{page_num}")
-    print(url)
-    res=url.content #url.text
-    #print(result)
+    res=result.content #url.text
     soup=BeautifulSoup(res,'html.parser')
     page_num += 1
     productTitle =soup.find_all('span',{'class':'a-size-base a-color-base'})#DEF ALL
